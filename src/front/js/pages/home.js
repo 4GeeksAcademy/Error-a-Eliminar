85a7bcb5ab7439144458e1d7c4eee9677d858a87
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-26 08:58:01 +0000

    creada la rama issue1

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,10 +9,13 @@ export const Home = () => {
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
+				
 			</p>
+
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
+
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
